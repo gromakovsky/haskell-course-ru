6b9a4f04487634c3f0578345ee57aaa718e16ad5
Ivan Gromakovsky <gie1994@gmail.com>
2014-11-09 00:56:15 +0300

    + some algebra

--- ITMOPrelude/Algebra.hs
+++ ITMOPrelude/Algebra.hs
@@ -19,5 +19,46 @@ class Monoid a => Group a where
     ginv :: a -> a
 
 -- Инстансы писать сюда
+instance Monoid Unit where
+    mempty = Unit
+    mappend _ _ = Unit
 
+instance Group Unit where
+    ginv _ = Unit
+
+instance (Monoid a, Monoid b) => Monoid (Pair a b) where
+    mempty = Pair mempty mempty
+    mappend a b = Pair {fst = fst a `mappend` fst b,
+                        snd = snd b `mappend` snd b}
+
+instance (Monoid a) => Monoid (Maybe a) where
+    mempty = Just mempty
+    mappend (Just a) (Just b) = Just $ mappend a b
+    mappend _ _ = Nothing
+
+instance Monoid Bool where
+    mempty = False
+    mappend = (||)
+
+instance Monoid Nat where
+    mempty = natZero
+    mappend = (+.)
+
+instance Monoid Int where
+    mempty = intZero
+    mappend = (.+.)
+
+instance Group Int where
+    ginv = intNeg
+
+instance Monoid Rat where
+    mempty = ratZero
+    mappend = (%*)
+
+instance Group Rat where
+    ginv = ratInv
+
+instance Monoid (List a) where
+    mempty = Nil
+    mappend = (++)
 

ff54fc6c810734b1cfdf23b6bae7defd34339451
Ivan Gromakovsky <gie1994@gmail.com>
2014-11-22 16:35:20 +0300

    + IO

--- ITMOPrelude/IO.hs
+++ ITMOPrelude/IO.hs
@@ -5,18 +5,21 @@ import ITMOPrelude.Primitive
 import ITMOPrelude.List
 import ITMOPrelude.Categories
 
+import Prelude (Show)
+
 data RealWorld = RealWorld
     { stdIn :: List Nat
     , stdOut :: List Nat
-    , exitCode :: Nat }
+    , exitCode :: Nat } deriving Show
 
 type IO a = State RealWorld a
 
 getNat :: IO Nat
-getNat = ?
+getNat = State $ \rw -> let RealWorld (Cons x xs) out exit = rw in (RealWorld xs out exit, x)
 
 putNat :: Nat -> IO ()
-putNat = ?
+putNat x = State $ \rw -> let RealWorld input output exit = rw in (RealWorld input (output `append` x) exit, ())
 
 setExitCode :: Nat -> IO ()
-setExitCode = ?
+setExitCode x = State $ \rw -> let RealWorld input output _ = rw in (RealWorld input output x, ())
+

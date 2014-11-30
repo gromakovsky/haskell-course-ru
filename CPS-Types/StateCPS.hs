b15c4ff49981d8dd04808b594c3fada42aa6e8fd
Ivan Gromakovsky <gie1994@gmail.com>
2014-12-01 01:22:55 +0300

    Added monad instance for StateCPS

--- CPS-Types/StateCPS.hs
+++ CPS-Types/StateCPS.hs
@@ -1,29 +1,29 @@
-{-# LANGUAGE FlexibleInstances #-}
+{-# LANGUAGE FlexibleInstances, RankNTypes #-}
 module StateCPS where
 
 import Control.Applicative
 import Control.Monad
 
-newtype State' r s a = State' {runState' :: ((s -> (s, a)) -> r) -> r}
+newtype State' s a = State' {runState' :: forall r . s -> (s -> a -> r) -> r}
 
-caseState' :: (State' r s a) -> ((s -> (s, a)) -> r) -> r
-caseState' = \x -> \f -> runState' x f
+caseState' :: (State' s a) -> ((s -> (s, a)) -> r) -> r
+caseState' = \x -> \f -> f (\s -> runState' x s (\s -> \a -> (s, a)))
 
 -- конструктор
-constrState' :: (s -> (s, a)) -> State' r s a
-constrState' st = State' $ \f -> f st
+state' :: (s -> (s, a)) -> State' s a
+state' st = State' $ \s -> \f -> let (s', a) = st s in f s' a
 
 -- инстансы
-instance Functor (State' r s) where
-    fmap f sa = State' $ \g -> caseState' sa (\st -> g $ \s -> let (s', a) = st s in (s', f a))
+instance Functor (State' s) where
+    fmap f sa = State' $ \s -> \g -> caseState' sa (\st -> let (s', a) = st s in g s' (f a))
 
-instance Applicative (State' r s) where
-    pure a = constrState' $ \s -> (s, a)
+instance Applicative (State' s) where
+    pure a = state' $ \s -> (s, a)
     sf <*> sa = undefined
 --    sf <*> sa = State' $ \g -> caseState' sa (\st -> caseState' sf (\f -> g $ \s -> let (s', a) = st s in (s', f a)))
 
-instance Monad (State' r s) where
-    return a = constrState' $ \s -> (s, a)
-    sa >>= f = undefined
+instance Monad (State' s) where
+    return a = state' $ \s -> (s, a)
+    sa >>= f = State' $ \s -> \g -> caseState' sa (\st -> let (s', a) = st s in runState' (f a) s' g)
 --    sa >>= f = State' $ \g -> caseState' sa (\st -> g $ \s -> let (s', a) = st s in f a (\st' -> st' s'))
 
